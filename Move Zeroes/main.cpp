b36d321ab3a569842901cfd70eab7a1a02e78c73
zhsh <sioxas@qq.com>
2018-08-19 00:14:59 +0800

    283. Move Zeroes

--- Move Zeroes/main.cpp
+++ Move Zeroes/main.cpp
@@ -1,7 +1,11 @@
 /**
+ * 283. Move Zeroes
+ * https://leetcode.com/problems/move-zeroes/description/
+ * 
  * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
  *
- * For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+ * For example, given nums = [0, 1, 0, 3, 12], 
+ * after calling your function, nums should be [1, 3, 12, 0, 0].
  *
  * Note:
  * You must do this in-place without making a copy of the array.
@@ -13,7 +17,7 @@
 
 using namespace std;
 
-void moveZeroes(vector<int>& nums) {
+void slowMoveZeroes(vector<int>& nums) {
     if(nums.size()<=1)return;
     int n=0;
     for(vector<int>::iterator it=nums.begin();it!=nums.end();){
@@ -33,8 +37,21 @@ void moveZeroes(vector<int>& nums) {
         nums.push_back(0);
 }
 
+void moveZeroes(vector<int>& nums) {
+    vector<int>::iterator zero_start = nums.begin(), zero_end = nums.begin();
+    while(zero_end!=nums.end()){
+        if(*zero_end == 0){
+            zero_end++;
+        }else{
+            *zero_start++ = *zero_end++;
+        }
+    }
+    while(zero_start!=zero_end)
+        *zero_start++ = 0;
+}
+
 int main() {
-    vector<int> nums = {0,0,1};
+    vector<int> nums = {0,0,1,0,3,0,3,0,0};
     moveZeroes(nums);
     for(auto v:nums)
         cout<<v<<" ";
