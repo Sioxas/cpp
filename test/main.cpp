568a62e17ef8dc727bf6023463727190524481b5
zhsh <sioxas@qq.com>
2018-08-22 22:41:33 +0800

    Update vscode settings

--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 int main(){
     cout << "hello vscode" << endl;
-    vector<int> v = {1,2,3,4,5,6,7,8,9,10};
-    for(const int i:v){
+    vector<int> nums = {1,2,3,4,5,6,0,0,0,0,7,7,7,8,8,8,9,9,10};
+    // vector<int>::iterator ver = unique(v.begin(), v.end());
+    vector<int>::iterator vend = nums.end();
+    for (vector<int>::iterator i = nums.begin(); i != vend; i++) {
+        if(*i==0){
+            *(vend - 1) = move(*i);
+        }
+    }
+    for(const int i:vector<int>(nums.begin(),vend)){
         cout << i << ' ';
     }
     size_t t = 0;
