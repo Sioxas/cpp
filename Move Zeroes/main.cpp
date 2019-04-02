f062b2e4278a8b9f1bee3f483f2fa1fb423be0c4
zhsh <sioxas@qq.com>
2019-04-02 11:39:37 +0800

    [283] Move Zeroes

--- Move Zeroes/main.cpp
+++ Move Zeroes/main.cpp
@@ -1,15 +1,31 @@
-/**
- * 283. Move Zeroes
+/*
+ * @lc app=leetcode id=283 lang=cpp
+ *
+ * [283] Move Zeroes
+ *
  * https://leetcode.com/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (53.36%)
+ * Total Accepted:    439.3K
+ * Total Submissions: 815.1K
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * Given an array nums, write a function to move all 0's to the end of it while
+ * maintaining the relative order of the non-zero elements.
  * 
- * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
- *
- * For example, given nums = [0, 1, 0, 3, 12], 
- * after calling your function, nums should be [1, 3, 12, 0, 0].
- *
+ * Example:
+ * 
+ * 
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * 
  * Note:
+ * 
+ * 
  * You must do this in-place without making a copy of the array.
  * Minimize the total number of operations.
+ * 
  */
 #include <iostream>
 #include <vector>
