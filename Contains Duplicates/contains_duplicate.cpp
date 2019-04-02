b1e32d0225dbb21f6bf2a63b7c6bf34485f08fa0
zhsh <sioxas@qq.com>
2019-04-02 11:03:55 +0800

    [217] Contains Duplicate

--- Contains Duplicates/contains_duplicate.cpp
+++ Contains Duplicates/contains_duplicate.cpp
@@ -1,35 +1,60 @@
-/**
- * 217. Contains Duplicate  QuestionEditorial Solution  My Submissions
+/*
+ * @lc app=leetcode id=217 lang=cpp
  *
- * Difficulty: Easy
+ * [217] Contains Duplicate
+ *
+ * https://leetcode.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (50.43%)
+ * Total Accepted:    314.9K
+ * Total Submissions: 614.6K
+ * Testcase Example:  '[1,2,3,1]'
  *
  * Given an array of integers, find if the array contains any duplicates.
- *
- * Your function should return true if any value appears at least twice in the array,
- * and it should return false if every element is distinct.
+ * 
+ * Your function should return true if any value appears at least twice in the
+ * array, and it should return false if every element is distinct.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3,1]
+ * Output: true
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4]
+ * Output: false
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ * 
  */
-#include <iostream>
-#include <vector>
-#include <map>
-#include <cstdlib>
-
+#include<unordered_set>
+#include<vector>
 using namespace std;
 
-bool containsDuplicate(vector<int>& nums) {
-    map<int,bool> count;
-    for(int v:nums){
-
-        if(count.find(v)!=count.end())
-            return true;//如果存在
-        else
-            count[v]=false;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for(int x:nums){
+            if(set.count(x)){
+                return true;
+            }
+            set.insert(x);
+        }
+        return false;
     }
-    return false;
-}
+};
 
-int main() {
-    vector<int> nums={3,3};
-    cout<<containsDuplicate(nums)<<endl;
-    system("pause");
+int main(){
+
     return 0;
 }
+
